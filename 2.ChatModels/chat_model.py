2bbbc4dfd632307f49cf4653fe5ff9a357ef1c64
Gautam-2604 <gaurisariag@gmail.com>
2026-01-02 15:24:00 +0530

    closed sourc3

---
+++ 2.ChatModels/chat_model.py
@@ -0,0 +1,10 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+
+llm = ChatOpenAI(model="allenai/olmo-3.1-32b-think:free", api_key=getenv("OPENROUTER_API_KEY"),base_url="https://openrouter.ai/api/v1",)
+
+answer = llm.invoke("What is the capital of India ??")
+print(answer)
